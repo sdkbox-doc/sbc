4ef48357438a1a19783425943deec408d9d10e85
hugo <hugohuang1111@gmail.com>
2021-02-05 14:26:47 +0800

    checkout gh-pages

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# ACCESS_TOKEN=$1
-
-echo $ACCESS_TOKEN
-
 # vuepress dist folder
 cd $GITHUB_WORKSPACE/docs/.vuepress/dist
 
@@ -16,11 +12,13 @@ git config user.email "hugohuang1111@gmail.com" --local
 # Generate a CNAME file
 # echo $CNAME > CNAME
 
-DEPLOY_REPO="https://${ACCESS_TOKEN}@github.com/sdkbox-doc/sbc-doc.git"
+# DEPLOY_REPO="https://${ACCESS_TOKEN}@github.com/sdkbox-doc/sbc-doc.git"
+DEPLOY_REPO="https://github.com/sdkbox-doc/sbc-doc.git"
 
+git chckout -b gh-pages
 git add .
 git commit -m "Deploy"
-git push --force $DEPLOY_REPO master:gh-pages
+git push --force $DEPLOY_REPO gh-pages
 rm -fr .git
 
 cd $GITHUB_WORKSPACE
