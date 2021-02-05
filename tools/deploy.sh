567f79fea64bb9ba03a43c36c677ff56778c44b7
hugo <hugohuang1111@gmail.com>
2021-02-05 11:22:12 +0800

    fix url

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -12,7 +12,7 @@ git config user.email "hugohuang1111@gmail.com"
 # Generate a CNAME file
 # echo $CNAME > CNAME
 
-DEPLOY_REPO="https://${ACCESS_TOKEN}@github.com/sbc-doc.git"
+DEPLOY_REPO="https://${ACCESS_TOKEN}@github.com/sdkbox-doc/sbc-doc.git"
 
 git add .
 git commit -m "Deploy"
